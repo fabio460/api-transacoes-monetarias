38699bbd5ce8b60a936d8b8b75dd0b2514f22250
unknown <dasilvadeoliveirafabio@gmail.com>
2022-11-25 23:35:31 -0300

    teste de deploy 3

--- controllers/controllerAccounts.ts
+++ controllers/controllerAccounts.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 import { handleDate } from "../uteis"
 
 export const getAccount =async (req:Request,res:Response)=>{
+   try {
     const account = await prisma.accounts.findUnique({
         where:{
             id:req.body.id 
@@ -15,6 +16,9 @@ export const getAccount =async (req:Request,res:Response)=>{
         }
     })
     res.json(account)
+   } catch (error) {
+    res.json(error)
+   }
 }
 
 export const cashOut =async (req:Request,res:Response)=>{
@@ -80,6 +84,7 @@ export const cashOut =async (req:Request,res:Response)=>{
 }
 
 export const getTransactions=async (req:Request,res:Response)=>{
+  try {
     const {id,selectedBy,day,month,year} = req.body
     var transaction = null
     switch (selectedBy) {
@@ -287,4 +292,7 @@ export const getTransactions=async (req:Request,res:Response)=>{
     var dates = transaction
 
     res.json(dates)
+  } catch (error) {
+    res.json(error)
+  }
 }
