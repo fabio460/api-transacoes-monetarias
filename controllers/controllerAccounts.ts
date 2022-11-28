846e75d16689a59856aff03d3e1d9b0bfcbe5552
unknown <dasilvadeoliveirafabio@gmail.com>
2022-11-28 10:57:31 -0300

    atualizações

--- controllers/controllerAccounts.ts
+++ controllers/controllerAccounts.ts
@@ -69,7 +69,7 @@ export const cashOut =async (req:Request,res:Response)=>{
                             value
                         }
                     })
-                    res.json("enviado o valor de "+value.toFixed(2)+" reais para "+ userIn?.username+" seu novo saldo é: "+newBalanceOut.toFixed(2)+" reais.")            
+                    res.json("enviado o valor de R$ "+value.toFixed(2)+" para "+ userIn?.username+" seu novo saldo é: R$"+newBalanceOut.toFixed(2)+".")            
                 }
             } else {
                 res.json('saldo insuficiente')
